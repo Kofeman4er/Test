5fb401c726431809163ae5f7f3e31209b8d6d1db
Kofeman4er <48658085+Kofeman4er@users.noreply.github.com>
2024-10-14 11:54:14 -0600

    Add files via upload

---
+++ main4.js
@@ -0,0 +1,10 @@
+// Create header
+
+
+
+/*var form = document.getElementById("mainForm");
+var text = document.getElementById("text"); // probably not a very good id!
+form.addEventListener("submit",function(e){ 
+    text.innerHTML = "Hello!";
+    e.preventDefault();
+});*/
